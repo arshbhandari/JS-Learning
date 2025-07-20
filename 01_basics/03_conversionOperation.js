c5f72f15c849595a179a9a625174c7604fc342fd
arshbhandari <167616645+arshbhandari@users.noreply.github.com>
2025-07-20 13:05:01 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let value = null
+
+//console.log(typeof value)
+// console.log(typeof (value))
+
+let valueInNumber = Number(value)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1 , flase=> 0
+// null =>0 , udefined => NaN , "arsh " => NaN
+
+let isLoggedIn = "arsh"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn)
+
+// 1=> true , 0 =>false
+// "" => false
+// "arsh" => true
+
+let score = 33
+
+let stringNumber = String(score)
+console.log(stringNumber)
+console.log(typeof stringNumber);
