a96b05d791fd31b7aa42c4cbd4dad907031a1fe4
arshbhandari <167616645+arshbhandari@users.noreply.github.com>
2025-07-20 18:02:24 +0530

    Add files via upload
    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 133424
+let accountEmail = "aesh34@123"
+var accountPassword = "123456"
+accountCity = "Uttarakhand"
+
+let accountState
+// accountId = 2
+accountEmail = "hc@hc.com"
+accountPassword = "5543556"
+accountCity = "Up"
+
+console.log(accountId);
+console.table([accountId ,accountEmail, accountPassword,accountCity, accountState])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3);  // we are using nodejs, not browser 
+
+console.log( 3 + 3 )//maintain code readibility
+console.log("Arsh")
+
+let name = "arsh"
+let age = 19
+let isloggedIn = false
+
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standelone value
+// undefined => value not assigned yet 
+// symbol => uniqueness 
+
+// object 
+
+console.log(typeof"arsh")
+console.log(typeof age)
+console.log(typeof null)  //object
+console.log(typeof undefined)  // undefined
