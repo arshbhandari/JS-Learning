b850483ad21d83f94f4c954e6eb2e98079fc04cf
arshbhandari <167616645+arshbhandari@users.noreply.github.com>
2025-09-10 12:28:43 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// ********** while **********
+
+
+// let index =0;
+// while(index <=10){
+//     console.log(`value : ${index}`);
+//     index = index+3;
+// }
+
+
+let myArray = ["flash","batman","superman"]
+// let arr =0;
+// while(arr <= myArray.length){
+//     console.log(`value : ${myArray[arr]}`);
+//     arr = arr+1;
+// }
+
+
+// (************ do while ************)
+
+let score = 9
+do{
+    console.log(`Score is : ${score}`);
+    score += 1;
+}while(score<= 10)
